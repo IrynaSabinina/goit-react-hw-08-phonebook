bd5aeb51eb40b8ec74917e5f7ca972ff9b425873
Iryna <pysarenkoiryna@gmail.com>
2022-08-15 12:42:05 +0300

    up

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,8 +1,6 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import { HomePage } from 'pages/HomePage/HomePage';
 
-import { Outlet } from 'react-router';
-
 import { Main } from './Main/Main';
 import { Sidebar } from './Sidebar/Sidebar';
 
